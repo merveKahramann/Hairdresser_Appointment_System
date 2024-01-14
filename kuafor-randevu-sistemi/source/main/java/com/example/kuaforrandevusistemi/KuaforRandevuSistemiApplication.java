228b2801ad48e08d14e96dd3e8489e4c264934da
merveKahraman <you@example.com>
2024-01-15 00:19:46 +0300

    Admin panel eklendi. kuaförlerin giriş sayfası güncellendi

--- kuafor-randevu-sistemi/source/main/java/com/example/kuaforrandevusistemi/KuaforRandevuSistemiApplication.java
+++ kuafor-randevu-sistemi/source/main/java/com/example/kuaforrandevusistemi/KuaforRandevuSistemiApplication.java
@@ -1,6 +1,6 @@
 package com.example.kuaforrandevusistemi;
 
-import org.springframework.boot.SpringApplication;
+import   org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
